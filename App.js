503365d8c15cd6b013291d2223199b1ef8226d04
Tam <studio@tamlove.com>
2021-06-18 20:31:18 +0100

    Added some style to AddEntry screen.

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import reducer from './reducers'
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View>
+      <View style={{flex: 1}}>
         <AddEntry />
       </View>
     </Provider>
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
@@ -9,11 +9,12 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { purple, white } from '../utils/colors'
 
 function SubmitBtn({ onPress }) {
     return (
-        <TouchableOpacity onPress={onPress}>
-            <Text>Submit</Text>
+        <TouchableOpacity style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn} onPress={onPress}>
+            <Text style={styles.submitBtnText}>Submit</Text>
         </TouchableOpacity>
     )
 }
@@ -86,10 +87,10 @@ class AddEntry extends Component {
         const metaInfo = getMetricMetaInfo()
         if(this.props.alreadyLogged) {
             return (
-                <View>
-                    <Ionicons name='ios-happy-outline' size={100} />
+                <View style={styles.center}>
+                    <Ionicons name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy-outline'} size={100} />
                     <Text>You already logged your information for today.</Text>
-                    <TextButton onPress={this.reset}>
+                    <TextButton style={{padding: 10}} onPress={this.reset}>
                         Reset
                     </TextButton>
                 </View>
@@ -97,13 +98,13 @@ class AddEntry extends Component {
         }
 
         return(
-            <View>
+            <View style={styles.container}>
                 <DateHeader date={(new Date()).toLocaleDateString()} />
                 {Object.keys(metaInfo).map((key) => {
                     const { getIcon, type, ...rest } = metaInfo[key]
                     const value = this.state[key]
                     return (
-                        <View key={key}>
+                        <View key={key} style={styles.row}>
                             {getIcon()}
                             {type === 'slider' ? <UdaciSlider value={value} onChange={(value) => this.slide(key, value)} {...rest} /> : 
                             <UdaciSteppers value={value} onIncrement={() => this.increment(key)} onDecrement={() => this.decrement(key)} {...rest} />}
@@ -115,6 +116,50 @@ class AddEntry extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20, 
+        backgroundColor: white,
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10, 
+        borderRadius: 7, 
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+    androidSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10, 
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 2, 
+        height: 45,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
+    },
+    center: {
+       flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30, 
+        marginRight: 30,
+    }
+})
 
 function mapStateToProps(state) {
     const key = timeToString()
